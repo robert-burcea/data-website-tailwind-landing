118fad9d28c4cfb22aa5b6d47d8fe82e55833efe
rburcea <robert.burcea@gmail.com>
2022-08-31 19:48:47 +0300

    change

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -5,7 +5,7 @@ function Analytics() {
   return (
     <div className="w-full bg-white py-16 px-4">
         <div className="max-w-[1240px] mx-auto grid  md:grid-cols-2">
-            <img className="w-[500px] mx-auto mx-auto my-4" src={Laptop} alt="Laptop"></img>
+            <img className="w-[500px] mx-auto my-4" src={Laptop} alt="Laptop"></img>
             <div className="flex flex-col justify-center">
                 <p className="text-[#00df9a] font-bold">
                     DATA ANALYTICS DASHBOARD
