266a553cb09989851bbfc2abfecc831f025c5e34
rburcea <robert.burcea@gmail.com>
2022-08-31 20:51:59 +0300

    create Newsletter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Analytics from './components/Analytics'
+import Newsletter from './components/Newsletter'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   );
 }
